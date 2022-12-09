967c8ff991d769279c97ce4edacb1cca419dc3bf
svalencia014 <sv.samuel.valencia014@gmail.com>
2022-12-09 18:36:04 -0500

    update

--- script/popup.js
+++ script/popup.js
@@ -1,1 +1,35 @@
 console.log("This is a popup");
+let pauseBtn = document.getElementsByClassName("toggle");
+let connectBtn = document.getElementsByClassName("connect");
+let urlBox = document.getElementById("cider");
+
+var url = "0.0.0.0"
+var init = false;
+
+window.addEventListener("load", (event) => {
+    chrome.cookies.get({
+        url: "http://localhost",
+        name: "cider.url",
+    });
+})
+
+connectBtn[0].addEventListener("click", function onClick() {
+    console.log("Connect button clicked");
+    url = urlBox.value;
+    console.log(`Connecting to ${url}`);
+    init = true;
+    connectBtn.item(0).innerHTML = "Connected";
+});
+
+pauseBtn[0].addEventListener("click", function onClick() {
+    if (init == false) {
+        console.log("Not connected to Cider");
+        alert("Not connected to Cider");
+        return;
+    }
+    console.log("Pause button clicked");
+    var XmlHttp = new XMLHttpRequest();
+    url = "http://" + url + ":9000/api/playback/playpause";
+    console.log(url);
+    XmlHttp.open("GET", url, false);
+});
